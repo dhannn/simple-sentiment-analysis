562326923512d4d9ed9da2aee4d3c90dca4b50e3
RichterDelaCruz <angelorichter.delacruz@gmail.com>
2023-04-16 19:04:43 +0800

    SVC done

--- src/svm.py
+++ src/svm.py
@@ -4,5 +4,8 @@ from sklearn.svm import SVC
     Takes a features of training set (X) and labels of training set (y)
     and returns the multinomial SVC
 '''
-def naive_bayes(X_train: pd.DataFrame, y_train: pd.DataFrame) -> SVC:
-    pass
+def support_vector_machine(X_train: pd.DataFrame, y_train: pd.DataFrame) -> SVC:
+    svc = SVC(kernel='linear', C=1.0, random_state=42) # initialize SVC with linear kernel and default parameters
+    svc.fit(X_train, y_train) # train the SVC on the training set
+    return svc # return the trained SVC model
+
