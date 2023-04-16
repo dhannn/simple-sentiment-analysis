3b497d3622e40c7afbc935d224b2353eb570efc1
RichterDelaCruz <angelorichter.delacruz@gmail.com>
2023-04-16 15:34:36 +0800

    Added the preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,21 @@
 import pandas as pd
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
-'''
-    Assume that 'text' field to be processed
-'''
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    print('Hello, world')
+    # Remove unnecessary characters
+    df['text'] = df['text'].apply(lambda x: re.sub('[^a-zA-Z0-9\s]', '', x))
+    
+    # Tokenization
+    df['text'] = df['text'].apply(lambda x: x.split())
+    
+    # Stopword removal
+    stop_words = set(stopwords.words('english'))
+    df['text'] = df['text'].apply(lambda x: [word for word in x if word.lower() not in stop_words])
+    
+    # Stemming
+    stemmer = PorterStemmer()
+    df['text'] = df['text'].apply(lambda x: [stemmer.stem(word) for word in x])
+    
+    return df
