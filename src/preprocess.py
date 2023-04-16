4fab07058e4e894c4844eac7e3383a172edf8814
RichterDelaCruz <angelorichter.delacruz@gmail.com>
2023-04-16 15:39:39 +0800

    Removed the tokenization process

--- src/preprocess.py
+++ src/preprocess.py
@@ -7,9 +7,6 @@ def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     # Remove unnecessary characters
     df['text'] = df['text'].apply(lambda x: re.sub('[^a-zA-Z0-9\s]', '', x))
     
-    # Tokenization
-    df['text'] = df['text'].apply(lambda x: x.split())
-    
     # Stopword removal
     stop_words = set(stopwords.words('english'))
     df['text'] = df['text'].apply(lambda x: [word for word in x if word.lower() not in stop_words])
