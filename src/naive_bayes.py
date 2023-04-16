9d825d2df6dde7ab43707a43743be805e260b481
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2023-04-16 16:00:52 +0800

    Add assignment for Naive Bayes

---
+++ src/naive_bayes.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
+
+'''
+    Takes a features of training set (X) and labels of training set (y)
+    and returns the multinomial Naive Bayes classifier 
+'''
+def naive_bayes(X_train: pd.DataFrame, y_train: pd.DataFrame) -> MultinomialNB:
+    pass
