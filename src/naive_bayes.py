20b695dd64c8736d6b4fb14cc27eace2282334ff
nicpjocson <nicpjocson@gmail.com>
2023-04-16 16:41:34 +0800

    naive_bayes func

--- src/naive_bayes.py
+++ src/naive_bayes.py
@@ -2,8 +2,12 @@ import pandas as pd
 from sklearn.naive_bayes import MultinomialNB
 
 '''
-    Takes a features of training set (X) and labels of training set (y)
+    Takes a features of training set (X), labels of training set (y)
     and returns the multinomial Naive Bayes classifier 
 '''
 def naive_bayes(X_train: pd.DataFrame, y_train: pd.DataFrame) -> MultinomialNB:
-    pass
+    
+    # Train the model on the training data set
+    X_train, y_train = train_test_split(X, y, random_state = 15)
+    classifier = MultinomialNB()
+    classifier.fit(X_train, y_train)
